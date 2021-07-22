e5ab847d901eb0fd039280fc0d51dfbb5c72ab9f
karol marques <karoldm1579@gmail.com>
2021-07-22 11:34:58 -0300

    create model product

---
+++ src/models/Product.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Product extends Model {
+    static init(connection) {
+        super.init({
+            id_publisher: DataTypes.INTEGER,
+            id_categories: DataTypes.INTEGER,
+            name: DataTypes.STRING,
+            author: DataTypes.STRING,
+            description: DataTypes.TEXT,
+            price: DataTypes.FLOAT,
+            total_pages: DataTypes.INTEGER,
+            image_uri: DataTypes.STRING,
+            created_at: DataTypes.DATE
+        }, {
+            sequelize: connection
+        })
+    }
+    static associations(models) {
+        this.belongsTo(models.Publisher, { foreignKey: 'id_publisher', as: 'publisher' })
+        this.belongsTo(models.Categorie, { foreignKey: 'id_categories', as: 'categorie' })
+    }
+}
+
+module.exports = Product
