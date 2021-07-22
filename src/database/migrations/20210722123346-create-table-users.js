c18721cd31c08ae78d559b9d88187216fc09ba33
karol marques <karoldm1579@gmail.com>
2021-07-22 09:59:28 -0300

    create table users

--- src/database/migrations/20210722123346-create-table-users.js
+++ src/database/migrations/20210722123346-create-table-users.js
@@ -2,20 +2,50 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cpf: {
+        type: Sequelize.STRING(14),
+        allowNull: false,
+        unique: true
+      },
+      zip_code: {
+        type: Sequelize.STRING(9),
+        allowNull: true
+      },
+      is_admin: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: 0
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      }
+    });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
+
   }
 };
