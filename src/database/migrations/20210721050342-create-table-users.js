c9710c2baf25b39e2b4e0b72422105968c521e25
Tungsinn <gvnalamas@tutanota.com>
2021-07-22 03:10:16 -0300

    Table users

--- src/database/migrations/20210721050342-create-table-users.js
+++ src/database/migrations/20210721050342-create-table-users.js
@@ -2,20 +2,56 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      cpf: {
+        type: Sequelize.STRING(14),
+        allowNull: false,
+        unique: true,
+      },
+
+      zip_code: {
+        type: Sequelize.STRING(9),
+        allowNull: true,
+      },
+      
+      is_admin: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
   }
 };
