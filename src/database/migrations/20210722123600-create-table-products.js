e7f6b618274e46d799cc7352a3ff3a121a75de33
karol marques <karoldm1579@gmail.com>
2021-07-22 10:09:00 -0300

    create table products

--- src/database/migrations/20210722123600-create-table-products.js
+++ src/database/migrations/20210722123600-create-table-products.js
@@ -2,7 +2,57 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('products', { id: Sequelize.INTEGER });
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      id_publisher: {
+        type: Sequelize.INTEGER,
+        references: { model: 'publishers', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      id_categories: {
+        type: Sequelize.INTEGER,
+        references: { model: 'categories', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      total_pages: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      image_uri: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      }
+    });
 
   },
 
