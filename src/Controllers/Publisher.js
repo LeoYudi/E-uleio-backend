7634d00ff08cfcc3eb5120ab2b46b098bbb76b14
karol marques <karoldm1579@gmail.com>
2021-07-22 22:42:51 -0300

    create publisher controller

---
+++ src/Controllers/Publisher.js
@@ -0,0 +1,38 @@
+const Publisher = require('../models/Publisher');
+
+module.exports = {
+    async save(req, res) {
+        const { name } = req.body;
+
+        const publisher = await Publisher.create({ name });
+
+        return res.status(200).send(publisher);
+    },
+    async list(req, res) {
+        const publishers = await Publisher.findAll();
+
+        return res.status(200).send(publishers);
+    },
+    async edit(req, res) {
+        const { id_publisher } = req.params;
+
+        const { name } = req.body;
+
+        const publisher = await Publisher.findByPk(id_publisher);
+
+        const publisherUpdate = await publihser.update({ name });
+
+        return res.status(200).send(publisherUpdate);
+    },
+
+    async delete(req, res) {
+        const { id_publisher } = req.params;
+
+        const publisher = await Publisher.findByPk(id_publisher);
+
+        const publisherDeleted = await publisher.destroy();
+
+        return res.status(200).send({ msg: 'publisher deleted' });
+    }
+
+}
