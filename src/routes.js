09ea6e8da103bf4acb7d0fede482cbdd47b475d1
karol marques <karoldm1579@gmail.com>
2021-07-22 22:39:23 -0300

    create routes categories

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const CategoryController = require('./Controllers/CategoryController');
 const ProductController = require('./Controllers/ProductController');
 const routes = express.Router();
 
@@ -11,5 +12,9 @@ routes.get('/products', ProductController.list);
 routes.put('/products/:id_product', ProductController.edit);
 routes.delete('/products:id_product', ProductController.delete);
 
+routes.post('/categories', CategoryController.save);
+routes.get('/categories', CategoryController.list);
+routes.put('/categories/:id_category', CategoryController.edit);
+routes.delete('/categories:id_category', CategoryController.delete);
 
 module.exports = routes;
