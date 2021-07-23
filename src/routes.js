85ea92abf8955ba99922d87420ee397925701ded
karol marques <karoldm1579@gmail.com>
2021-07-22 22:24:20 -0300

    create routes products

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,10 @@ routes.get('/', (req, res) => {
   return res.status(200).json('Rota inicial');
 });
 
+routes.post('/products', ProductController.save);
+routes.get('/products', ProductController.list);
+routes.put('/products', ProductController.edit);
+routes.delete('/products', ProductController.delete);
+
+
 module.exports = routes;
