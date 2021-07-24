cc026f7a29ef45dee9571a926f7db822ed0507d6
Tungsinn <gvnalamas@tutanota.com>
2021-07-24 01:51:50 -0300

    Rotas /login e /users

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const routes = express.Router();
+const UserController = require('./controllers/UserController');
+const authMiddleware = require('./middlewares/auth');
 
 routes.get('/', (req, res) => {
   return res.status(200).json('Rota inicial');
 });
+
+routes.post('/login', UserController.login);
+routes.post('/users', UserController.save);
+routes.get('/users', authMiddleware, UserController.list);
+routes.put('/users', authMiddleware, UserController.edit);
+routes.delete('/users', authMiddleware, UserController.delete);
 
 module.exports = routes;
