caa7fc97b36688de494cd381c86e90e525d5c248
Tungsinn <gvnalamas@tutanota.com>
2021-07-24 03:19:09 -0300

    API pública com cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('dotenv').config();
 require('./database');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
