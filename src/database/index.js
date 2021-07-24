1a6eb47b7925959284ae7310f66708e55fe8506c
Tungsinn <gvnalamas@tutanota.com>
2021-07-24 01:46:04 -0300

    Fix

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
-const User = require('../models/Users');
+const User = require('../models/User');
 
 const connection = new Sequelize(dbConfig);
 
